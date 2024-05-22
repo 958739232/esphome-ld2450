edc8cf21d2973b611f4a4430f2b64e863042897e
958739232 <166364775+958739232@users.noreply.github.com>
2024-05-22 19:59:45 +0800

    Delete components/LD2450 directory

--- components/LD2450/tracking_mode_switch.cpp
+++
@@ -1,9 +0,0 @@
-#include "tracking_mode_switch.h"
-
-namespace esphome::ld2450
-{
-    void TrackingModeSwitch::write_state(bool state)
-    {
-        parent_->set_tracking_mode(state);
-    }
-} // namespace esphome::ld2450
